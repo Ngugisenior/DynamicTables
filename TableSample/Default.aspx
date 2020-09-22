01ce6318dd123df4334732024974ec90b64d8a81
Ngugisenior <jmuiruri@nilefeeds.com>
2020-09-22 10:11:25 +0200

    Innitial commit

---
+++ TableSample/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TableSample.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Table Test</h1>
+            <p>
+                Rows:
+                <asp:TextBox ID="TxtRows" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Collumns:&nbsp;
+                <asp:TextBox ID="TxtColumns" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                <asp:CheckBox ID="ChkBorder" runat="server" Text=" Put Border Around cells" />
+            </p>
+            <p>
+                <asp:Button ID="BtnCreate" runat="server" OnClick="BtnCreate_Click" Text="Create" />
+            </p>
+            <p>
+                &nbsp;</p>
+            <p>
+                <asp:Table ID="Table1" runat="server">
+                </asp:Table>
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TableSample/Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TableSample
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Counfigure the table's appearance.
+            Table1.BorderStyle = BorderStyle.Inset;
+            Table1.BorderWidth = Unit.Pixel(1);
+        }
+
+        protected void BtnCreate_Click(object sender, EventArgs e)
+        {
+            //Remove all the current rows and cells.
+            //this is an optional If EnableViewState property is set to false.
+            Table1.Controls.Clear();
+
+            //Create a new table row object
+            int numRows = Int32.Parse(TxtRows.Text);
+            int numColumns = Int32.Parse(TxtColumns.Text);
+
+            //Create a new tableRow object
+            for (int i = 0; i < numRows; i++)
+            {
+                TableRow newRow = new TableRow();
+
+                //Put the new tablerow into the table
+                Table1.Controls.Add(newRow);
+
+                for (int j = 0; j < numColumns; j++)
+                {
+                    //CreateChildControls a new cell object
+                    TableCell newCell = new TableCell();
+
+                    //Create a new label
+                    Label newLabel = new Label();
+                    newLabel.Text = "Example Cell (" + i.ToString() + "," + j.ToString() + ")<br/>";
+                    Image newImage = new Image();
+                    newImage.ImageUrl = @"~\cellpic.PNG";
+
+                    //Add the label and the picture to the cell
+                    newCell.Controls.Add(newLabel);
+                    newCell.Controls.Add(newImage);
+
+                    //Check if the user wants a border
+                    if (ChkBorder.Checked)
+                    {
+                        newCell.BorderStyle = BorderStyle.Inset;
+                        newCell.BorderWidth = Unit.Pixel(1);
+                    }
+
+                    //Put the tablecell in the TableRow
+                    newRow.Controls.Add(newCell);
+                }
+            }
+        }
+    }
+}
